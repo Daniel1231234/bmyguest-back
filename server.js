13d711141f16d8eadbf7dc96c20df6fc995c171d
Daniel Shechter <dshctr1993@gmail.com>
2022-11-10 13:59:38 +0200

    upload to render2

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.get("/**", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
-app.listen(port, () => {
+http.listen(port, () => {
   logger.info("Server is running on port: " + port)
 })
